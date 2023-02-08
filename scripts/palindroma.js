bd6b7fcd9e78a0e50e0dd509afcfe67909f6ef68
Blaser993 <corradodepinto.design@gmail.com>
2023-02-08 14:14:55 +0100

    FEAT chiedi all'utente di inserire una parola

--- scripts/palindroma.js
+++ scripts/palindroma.js
@@ -4,3 +4,7 @@ console.log("Palindroma")
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 // Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
+
+let parolaUtente = prompt("scrivi una parola");
+
+console.log(parolaUtente)
