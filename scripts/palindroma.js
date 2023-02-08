e6c91ca968e8eda37bf2c2d7a567be004cb76786
Blaser993 <corradodepinto.design@gmail.com>
2023-02-08 14:08:51 +0100

    START

---
+++ scripts/palindroma.js
@@ -0,0 +1,6 @@
+console.log("Palindroma")
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
---
+++ scripts/pariedispati.js
@@ -0,0 +1,8 @@
+console.log("Pari e dispari")
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Comunichiamo all’utente chi ha vinto. (decidete voi come)
