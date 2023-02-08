5eea52e450531eb37df0e47a06dfbea80afb7f0d
Blaser993 <corradodepinto.design@gmail.com>
2023-02-08 15:41:03 +0100

    FEAT scompone la parola dell'utente in lettere

--- scripts/palindroma.js
+++ scripts/palindroma.js
@@ -5,6 +5,36 @@ console.log("Palindroma")
 // Creare una funzione per capire se la parola inserita è palindroma
 // Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
 
+
+function palindroma (parolaUtente) {
+
+    const parolaPalindroma = parolaUtente === reverseParola
+    
+    console.log("la parola" , parolaUtente ,  "è palindroma")
+}
+
+
+
+
+
+
+
 let parolaUtente = prompt("scrivi una parola");
 
-console.log(parolaUtente)
+
+let parola = [parolaUtente]
+
+
+for (let i = 0; i < parolaUtente.length ; i++){
+
+    let lettera = i
+
+    lettera = parolaUtente[i]
+
+    console.log("questa è la lettera della parola inserita: ",lettera)  
+}
+
+
+
+
+
